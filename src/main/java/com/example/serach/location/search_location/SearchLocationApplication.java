b8ffedba73e82747cf4b5e9f474bb2aa714a7ea2
JaySabnis <jaysabnis2601@gmail.com>
2025-07-16 14:46:35 +0530

    feat(db-changes): connect to remote db.

--- src/main/java/com/example/serach/location/search_location/SearchLocationApplication.java
+++ src/main/java/com/example/serach/location/search_location/SearchLocationApplication.java
@@ -2,8 +2,9 @@ package com.example.serach.location.search_location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SearchLocationApplication {
 
 	public static void main(String[] args) {
