974f369e78a333276aff42785b647bfa32ea3d91
JaySabnis <jaysabnis2601@gmail.com>
2025-10-10 12:06:44 +0530

    allowed origins all selective sites

--- src/main/java/com/example/serach/location/search_location/config/AppCorsConfig.java
+++ src/main/java/com/example/serach/location/search_location/config/AppCorsConfig.java
@@ -2,10 +2,14 @@ package com.example.serach.location.search_location.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class AppCorsConfig {
@@ -13,7 +17,13 @@ public class AppCorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*"); // todo: later restrict to only one FE domain
+
+        List<String> allowedOrigins = Arrays.asList(
+                "http://localhost:8080",   // local frontend
+                "https://bookingapp1-2525.web.app/"  // production frontend
+        );
+
+        allowedOrigins.forEach(config::addAllowedOrigin); // your frontend
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
